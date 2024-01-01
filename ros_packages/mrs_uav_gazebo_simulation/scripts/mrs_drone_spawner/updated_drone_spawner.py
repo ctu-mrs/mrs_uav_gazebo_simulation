d7b11db209c171adf95de5c6c2b8705b7b177ab5
stibipet <stibinger@fly4future.com>
2024-01-01 18:53:12 +0100

    improve documentation and consistency

--- ros_packages/mrs_uav_gazebo_simulation/scripts/mrs_drone_spawner/updated_drone_spawner.py
+++ ros_packages/mrs_uav_gazebo_simulation/scripts/mrs_drone_spawner/updated_drone_spawner.py
@@ -17,13 +17,12 @@ from component_wrapper import ComponentWrapper
 from template_wrapper import TemplateWrapper
 
 
-TEMPLATE_SUFFIX = '.sdf.jinja'
 glob_running_processes = []
 
 # #{ filter_templates(template_name)
-def filter_templates(template_name):
+def filter_templates(template_name, suffix):
     '''Comparator used to load files with given suffix'''
-    return template_name.endswith(TEMPLATE_SUFFIX)
+    return template_name.endswith(suffix)
 # #}
 
 def custom_decoder(obj):
@@ -60,24 +59,49 @@ class MrsDroneSpawner:
     def __init__(self, show_help=False, verbose=False):
 
         self.verbose = verbose
-        rospack = rospkg.RosPack()
+
+        rospy.init_node('mrs_drone_spawner', anonymous=True)
 
         resource_paths = []
-        # TODO make this dynamic
+        rospack = rospkg.RosPack()
         resource_paths.append(rospack.get_path('mrs_uav_gazebo_simulation'))
-        resource_paths.append(rospack.get_path('external_gazebo_models'))
+
+        # # #{ load required params
+        try:
+            self.vehicle_base_port = rospy.get_param('~vehicle_base_port')
+            self.mavlink_tcp_base_port = rospy.get_param('~mavlink_tcp_base_port')
+            self.mavlink_udp_base_port = rospy.get_param('~mavlink_udp_base_port')
+            self.launch_base_port = rospy.get_param('~launch_base_port')
+            self.template_suffix = rospy.get_param('~template_suffix')
+        except KeyError as err:
+            rospy.logerr(f'[MrsDroneSpawner]: Could not load required param {err}')
+            raise rospy.ROSInterruptException
+        # # #}
+
+        # # #{ handle extra resource paths
+        extra_resource_paths = rospy.get_param('~extra_resource_paths', [])
+        for elem in extra_resource_paths:
+            if os.path.exists(elem):
+                rpath = elem
+            else:
+                rpath = rospack.get_path(elem)
+            rospy.loginfo(f'[MrsDroneSpawner]: Adding extra resources from {rpath}')
+            resource_paths.append(rpath)
 
         self.jinja_env = self.configure_jinja2_environment(resource_paths)
+        # # #}
 
         try:
             self.jinja_templates = self.build_template_database()
         except RecursionError as err:
-            print(f'[ERROR] [MrsDroneSpawner]: {err}')
+            rospy.logerr(f'[MrsDroneSpawner]: {err}')
             raise rospy.ROSInterruptException
 
-        print('Jinja templates available:')
-        print([name for name in self.jinja_templates.keys()])
-        print('------------------------')
+        rospy.loginfo(f'[MrsDroneSpawner]: ------------------------')
+        rospy.loginfo(f'[MrsDroneSpawner]: Jinja templates loaded:')
+        for name in self.jinja_templates.keys():
+            rospy.loginfo(f'[MrsDroneSpawner]: "{name}" from "{self.jinja_templates[name].jinja_template.filename}"')
+        rospy.loginfo(f'[MrsDroneSpawner]: ------------------------')
 
         # load all available models
         # from current package
@@ -99,18 +123,19 @@ class MrsDroneSpawner:
         Get all templates loaded by the given jinja environment
         :returns a list of tuples, consisting of (str_name, jinja2.Template)
         '''
-        template_names = self.jinja_env.list_templates(filter_func=filter_templates)
+        template_names = self.jinja_env.list_templates(filter_func=lambda template_name: filter_templates(template_name, self.template_suffix))
         templates = []
         for i, full_name in enumerate(template_names):
-            print(f'\t({i+1}/{len(template_names)}): {full_name}')
-            template_name = full_name.split(os.path.sep)[-1][:-(len(TEMPLATE_SUFFIX))]
+            rospy.loginfo(f'[MrsDroneSpawner]: \t({i+1}/{len(template_names)}): {full_name}')
+            template_name = full_name.split(os.path.sep)[-1][:-(len(self.template_suffix))]
             templates.append((template_name, self.jinja_env.get_template(full_name)))
         return templates
     # #}
 
-    # #{ get_template_imports(self, template)
-    def get_template_imports(self, template):
-        with open(template.filename, 'r') as f:
+    # #{ get_template_imports(self, jinja_template)
+    def get_template_imports(self, jinja_template):
+        '''Returns a list of sub-templates imported by a given jinja_template'''
+        with open(jinja_template.filename, 'r') as f:
             template_source = f.read()
             preprocessed_template = template_source.replace('\n', '')
             parsed_template = self.jinja_env.parse(preprocessed_template)
@@ -154,7 +179,7 @@ class MrsDroneSpawner:
                             for pair in elem.node.items:
                                 spawner_default_args[pair.key.value] = pair.value.value
                         else:
-                            print(f'Unsupported param type {type(elem.node)}')
+                            rospy.logwarn(f'[MrsDroneSpawner]: Unsupported param type "{type(elem.node)}" in template {template.filename}')
                     if isinstance(elem, jinja2.nodes.Assign) and elem.target.name == 'spawner_keyword':
                         spawner_keyword = elem.node.value
                 if spawner_keyword is not None:
@@ -164,6 +189,10 @@ class MrsDroneSpawner:
 
     # #{ get_all_available_components(self, template_wrapper, all_components)
     def get_all_available_components(self, template_wrapper, all_components):
+        '''
+        Recursive function to get all spawner-compatible components accessible from template_wrapper
+        Includes components in imported sub-templates
+        '''
         all_components.update(template_wrapper.components)
         for i in template_wrapper.imported_templates:
             try:
@@ -175,10 +204,16 @@ class MrsDroneSpawner:
 
     # #{ build_template_database(self)
     def build_template_database(self):
+        '''
+        Generate a database of jinja2 templates available to the spawner
+        Scans through all folders provided into the jinja2 environment for files with matching target suffix
+        Recursively checks templates imported by templates, prevents recursion loops
+        Returns a dictionary of TemplateWrapper objects in format {template_name: template_wrapper}
+        '''
 
         template_wrappers = {}
 
-        print('Loading all templates')
+        rospy.loginfo('[MrsDroneSpawner]: Loading all templates')
         all_templates = self.get_all_templates()
         for name, template in all_templates:
             imports = self.get_template_imports(template)
@@ -186,7 +221,7 @@ class MrsDroneSpawner:
             wrapper = TemplateWrapper(template, imports, components)
             template_wrappers[name] = wrapper
 
-        print('Reindexing imported templates')
+        rospy.loginfo('[MrsDroneSpawner]: Reindexing imported templates')
         for name, wrapper in template_wrappers.items():
             for i, it in enumerate(wrapper.imported_templates):
                 if not isinstance(it, TemplateWrapper):
@@ -194,14 +229,14 @@ class MrsDroneSpawner:
                         if ww.jinja_template == it:
                             wrapper.imported_templates[i] = ww
 
-        print('Adding available components from dependencies')
+        rospy.loginfo('[MrsDroneSpawner]: Adding available components from dependencies')
         for _, wrapper in template_wrappers.items():
             prev_limit = sys.getrecursionlimit()
             sys.setrecursionlimit(len(template_wrappers) + 1)
             wrapper.components = self.get_all_available_components(wrapper, {})
             sys.setrecursionlimit(prev_limit)
 
-        print('Template database built')
+        rospy.loginfo('[MrsDroneSpawner]: Template database built')
 
         return template_wrappers
     # #}
@@ -243,7 +278,7 @@ class MrsDroneSpawner:
             input_dict['help'] = False
 
         # parse out the keywords starting with '--'
-        pattern = re.compile(r"(--\S*)")
+        pattern = re.compile(r'(--\S*)')
         substrings = [m.strip() for m in re.split(pattern, input_str) if len(m.strip()) > 0]
 
         # before the first keyword, there should be only device IDs
@@ -263,15 +298,13 @@ class MrsDroneSpawner:
                 if len(input_keys) > 1:
                     input_dict[input_keys[-1]] = self.parse_string_to_objects(substrings[i])
 
-        # extract model name
+        input_dict['model'] = None
+        # attempt to match model to available templates
         for k in input_dict.keys():
             if k in self.jinja_templates.keys():
                 input_dict['model'] = str(k)
                 del input_dict[k]
                 break
-
-        if 'help' not in input_dict.keys() and 'model' not in input_dict.keys():
-            raise ValueError('Model name not defined. Specify a model with a keyword (e.g. "--x500")')
 
         return input_dict
     # #}
@@ -281,21 +314,23 @@ class MrsDroneSpawner:
         '''
         Attempt to convert input_str into a dictionary or a list
         Convert numerals into number datatypes whenever possible
+        Returns None if the input cannot be interpreted as dict or list
         '''
         input_str = input_str.strip()
         try:
             params = ast.literal_eval(input_str)
             if isinstance(params, dict):
                 return params
+
         except (SyntaxError, ValueError) as e:
-            # print(f"Error converting string {input_str} to dictionary: {e}")
+            # cannot convert input_str to a dict
             pass
 
         params = []
         for s in input_str.split():
             if len(s) > 0:
                 try:
-                    # try converting string to numbers
+                    # try to convert input_str to numbers
                     params.append(ast.literal_eval(s))
                 except ValueError:
                     # leave non-numbers as string
@@ -309,10 +344,19 @@ class MrsDroneSpawner:
 
     # #{ get_help_text(self, input_dict):
     def get_help_text(self, input_dict):
-        if 'help' not in input_dict.keys():
-            return
+        '''
+        Used to construct the help text (string) for a given dict of input args
+        Returns:
+            generic spawner help
+            or
+            help for a specific model
+            or
+            None (if the input does not contain "help")
+        '''
+        if not input_dict['help']:
+            return None
 
-        if 'model' not in input_dict.keys():
+        if input_dict['model'] is None:
             display_text = self.get_spawner_help_text()
         else:
             display_text = self.get_model_help_text(input_dict['model'])
@@ -322,13 +366,19 @@ class MrsDroneSpawner:
 
     # #{ get_model_help_text(self, model_name)
     def get_model_help_text(self, model_name):
-        print(f'Getting help for model {model_name}')
-        response = ''
+        '''
+        Create a help string by loading all components from a given template in the following format
+        Component name
+            Description:
+            Default args:
+        '''
+        rospy.loginfo(f'[MrsDroneSpawner]: Getting help for model {model_name}')
+        response = f'[MrsDroneSpawner]: Components available for model {model_name}:\n'
         try:
             template_wrapper = self.jinja_templates[model_name]
-            print(f'Template {template_wrapper.jinja_template.name} loaded with {len(template_wrapper.components)} components')
+            rospy.loginfo(f'[MrsDroneSpawner]: Template {template_wrapper.jinja_template.name} loaded with {len(template_wrapper.components)} components')
         except ValueError:
-            return f"Template for model {model_name} not found"
+            return f'Template for model {model_name} not found'
 
         for name, component in template_wrapper.components.items():
             response += f'{component.keyword}\n\tDescription: {component.description}\n\tDefault args: {component.default_args}\n\n'
@@ -338,11 +388,17 @@ class MrsDroneSpawner:
 
     # #{ get_spawner_help_text(self)
     def get_spawner_help_text(self):
-        print(f'Getting generic spawner help')
+        '''Create a generic help string for the spawner basic use'''
+
+        rospy.loginfo(f'[MrsDroneSpawner]: Getting generic spawner help')
         response = 'Expected input:\n'
         response += '\tdevice ids (integers separated by spaces)\n'
         response += '\tkeywords (specified in jinja components starting with \'--\')\n'
-        response += '\tcomponent args following a keyword (values separated by spaces or a python dictionary)'
+        response += '\tcomponent args following a keyword (values separated by spaces or a python dictionary)\n'
+        response += '\tavailable model templates: '
+        for model_name in self.jinja_templates.keys():
+            response += f'{model_name}, '
+        response += '\n'
         return response
     # #}
 
@@ -353,35 +409,42 @@ class MrsDroneSpawner:
 
     # #{ render(self, model_name, output)
     def render(self, model_name, output, spawner_args):
-        print('Rendering model', model_name)
-        for template_name, template_wrapper in self.jinja_templates.items():
-            if model_name == template_name:
-                print('Using template', template_name)
-                # spawner_args = {
-                #     "enable_component_with_args": [0.01, 0.38],
-                #     "enable_component_with_args_as_dict": {'roll': 0},
-                #     # "enable_second_order_component": None,
-                #     # "enable_third_order_component": None,
-                # }
+        '''
+        Renders a jinja template into a sdf file
+        :param model_name: name of the model, has to match a key in the self.jinja_templates database
+        :param output: full filepath to write the output into
+        :param spawner_args: spawner args specified by the user, will be passed into the template
+        '''
+        
+        params = {
+            "spawner_args": spawner_args
+            #     "name": "uav1",
+            #     "namespace": "uav1",
+            #     "enable_rangefinder": True,
+            #     "enable_component": True,
+            #     "spawner_args": {'roll': 0.1, 'pitch': 0.1}
+        }
 
-                params = {
-                    "spawner_args": spawner_args
-                    #     "name": "uav1",
-                    #     "namespace": "uav1",
-                    #     "enable_rangefinder": True,
-                    #     "enable_component": True,
-                    #     "spawner_args": {'roll': 0.1, 'pitch': 0.1}
-                }
-                context = template_wrapper.jinja_template.new_context(params)
-                rendered_template = template_wrapper.jinja_template.render(context)
-                root = xml.dom.minidom.parseString(rendered_template)
-                ugly_xml = root.toprettyxml(indent='  ')
-                # Remove empty lines
-                pretty_xml = "\n".join(line for line in ugly_xml.split("\n") if line.strip())
-                with open(output, 'w') as f:
-                    f.write(pretty_xml)
-                    print('Rendered model written to', output)
-                    return
+        try:
+            template_wrapper = self.jinja_templates[model_name]
+        except KeyError:
+            rospy.logerr(f'[MrsDroneSpawner]: Cannot render model "{model_name}". Template not found!')
+            return
+ 
+        rospy.loginfo(f'[MrsDroneSpawner]: Rendering model "{model_name}" using template {template_wrapper.jinja_template.filename}')
+
+        context = template_wrapper.jinja_template.new_context(params)
+        rendered_template = template_wrapper.jinja_template.render(context)
+        root = xml.dom.minidom.parseString(rendered_template)
+        ugly_xml = root.toprettyxml(indent='  ')
+        # Remove empty lines
+        pretty_xml = '\n'.join(line for line in ugly_xml.split('\n') if line.strip())
+        
+        with open(output, 'w') as f:
+            f.write(pretty_xml)
+            rospy.loginfo(f'[MrsDroneSpawner]: Rendered model written to {output}')
+            return
+
     # #} end render
 
 if __name__ == '__main__':
@@ -395,40 +458,14 @@ if __name__ == '__main__':
 
     try:
         spawner = MrsDroneSpawner(show_help, verbose)
+        spawner_args = spawner.parse_user_input('2 10 --enable_component_with_args 0.01 0.38 1E10 --enable_component_with_args_as_dict {"roll": 0, "pitch": 22, "aaa": "bbbb"} --enable-noargs --dummy')
 
-        # help_response = spawner.get_help_for_model('dummy')
-        # print(help_response)
-
-        # spawner_args = spawner.parse_input_params('--enable_component --enable_component_with_args 14')
-        # print(spawner_args)
-        # spawner_args = spawner.parse_user_input('--enable_component')
-        # spawner.parse_as_dict('{"aaa": "bbb", "ccc": 22, "ddd": {"u":"v", "w":1}, "eee": None}')
-
-        # spawner_args = spawner.parse_user_input('2 10 --enable_component_with_args 0.01 0.38 1E10 --enable_component_with_args_as_dict {"roll": 0, "pitch": 0, "aaa": "bbbb"} --enable-noargs')
-        spawner_args = spawner.parse_user_input('--help 2 10 --enable_component_with_args 0.01 0.38 1E10 --enable_component_with_args_as_dict {"roll": 0, "pitch": 22, "aaa": "bbbb"} --enable-noargs --dummy')
-
-        # for kw, args in spawner_args.items():
-        #     print(kw)
-        #     print('\t', args, type(args))
-        #     if isinstance(args, dict):
-        #         for e in args.values():
-        #             print('\t\t', e, type(e))
-        #     elif isinstance(args, list):
-        #         for e in args:
-        #             print('\t\t', e, type(e))
-        #     else:
-        #         print('\t\t', args)
-
-        # # params = jinja_utils.get_all_params('x555', spawner.jinja_env)
-        # # params = jinja_utils.get_all_params('f400', spawner.jinja_env)
-        # # print(params)
-        # # spawner.render('x555', '/home/mrs/devel_workspace/src/external_gazebo_models/models/x555/sdf/x555.sdf')
-        # # spawner.render('f400', '/home/mrs/devel_workspace/src/external_gazebo_models/models/f400/sdf/f400.sdf')
-
-        help_text = spawner.get_help_text(spawner_args)
-        if help_text is not None:
-            print(help_text)
+        if spawner.get_help_text(spawner_args) is not None:
+            rospy.loginfo(help_text)
+        elif spawner_args['model'] is not None:
+            spawner.render(spawner_args['model'], '/home/mrs/devel_workspace/src/external_gazebo_models/dummy.sdf', spawner_args)
         else:
-            spawner.render('dummy', '/home/mrs/devel_workspace/src/external_gazebo_models/dummy.sdf', spawner_args)
+            rospy.logerr('[MrsDroneSpawner]: Model not specified')
+            raise rospy.ROSInterruptException
     except rospy.ROSInterruptException:
         pass
