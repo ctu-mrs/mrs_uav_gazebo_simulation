bbfe34c31113dfc9b04062b755821ad922518f52
stibipet <stibinger@fly4future.com>
2024-01-01 18:55:03 +0100

    add test script for testing purposes

---
+++ ros_packages/mrs_uav_gazebo_simulation/test_launch.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+roslaunch mrs_uav_gazebo_simulation mrs_drone_spawner.launch custom_config:=/home/mrs/devel_workspace/src/mrs_uav_gazebo_simulation/ros_packages/mrs_uav_gazebo_simulation/config/spawner_custom_config.yaml
